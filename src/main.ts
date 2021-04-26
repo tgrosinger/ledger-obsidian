e03ce1f9fb5203e416d1b5b94631a10e09ed3049
Tony Grosinger <tony@grosinger.net>
2021-04-26 07:55:37 -0700

    Add settings tab

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,19 @@ import {
   formatExpense,
   getTransactionCache,
 } from './file-interface';
-import { billIcon } from './graphics';
+import { billIcon, buyMeACoffee, paypal } from './graphics';
 import type { TransactionCache } from './parser';
 import { ISettings, settingsWithDefaults } from './settings';
 import AddExpenseUI from './ui/AddExpenseUI.svelte';
 import type { default as MomentType } from 'moment';
-import { addIcon, Modal, Plugin, TAbstractFile } from 'obsidian';
+import {
+  addIcon,
+  Modal,
+  Plugin,
+  PluginSettingTab,
+  Setting,
+  TAbstractFile,
+} from 'obsidian';
 
 declare global {
   interface Window {
@@ -25,6 +32,7 @@ export default class LedgerPlugin extends Plugin {
     console.log('ledger: Loading plugin v' + this.manifest.version);
 
     await this.loadSettings();
+    this.addSettingTab(new SettingsTab(this));
 
     addIcon('ledger', billIcon);
     this.addRibbonIcon('ledger', 'Add to Ledger', () => {
@@ -100,3 +108,92 @@ class AddExpenseModal extends Modal {
     contentEl.empty();
   };
 }
+
+class SettingsTab extends PluginSettingTab {
+  private readonly plugin: LedgerPlugin;
+
+  constructor(plugin: LedgerPlugin) {
+    super(plugin.app, plugin);
+    this.plugin = plugin;
+  }
+
+  public display(): void {
+    const { containerEl } = this;
+    containerEl.empty();
+
+    containerEl.createEl('h2', { text: 'Ledger Plugin - Settings' });
+
+    new Setting(containerEl)
+      .setName('Currency Symbol')
+      .setDesc('Prefixes all transaction amounts')
+      .addText((text) => {
+        text.setPlaceholder('$').setValue(this.plugin.settings.currencySymbol);
+        text.inputEl.onblur = (e: FocusEvent) => {
+          this.plugin.settings.currencySymbol = (e.target as HTMLInputElement).value;
+          this.plugin.saveData(this.plugin.settings);
+        };
+      });
+
+    new Setting(containerEl)
+      .setName('Ledger File')
+      .setDesc('Path in the Vault to your Ledger file. Must be a .md file.')
+      .addText((text) => {
+        text
+          .setValue(this.plugin.settings.ledgerFile)
+          .setPlaceholder('Ledger.md');
+        text.inputEl.onblur = (e: FocusEvent) => {
+          this.plugin.settings.ledgerFile = (e.target as HTMLInputElement).value;
+          this.plugin.saveData(this.plugin.settings);
+        };
+      });
+
+    new Setting(containerEl)
+      .setName('Include final line amount')
+      .setDesc(
+        'Include the dollar amount on the final line of a transaction. This value is optional, and is alway equal to the sum of the previous lines * -1.',
+      )
+      .addToggle((toggle) => {
+        toggle
+          .setValue(this.plugin.settings.includeFinalLineAmount)
+          .onChange((value) => {
+            this.plugin.settings.includeFinalLineAmount = value;
+            this.plugin.saveData(this.plugin.settings);
+          });
+      });
+
+    const div = containerEl.createEl('div', {
+      cls: 'ledger-donation',
+    });
+
+    const donateText = document.createElement('p');
+    donateText.appendText(
+      'If this plugin adds value for you and you would like to help support ' +
+        'continued development, please use the buttons below:',
+    );
+    div.appendChild(donateText);
+
+    const parser = new DOMParser();
+
+    div.appendChild(
+      createDonateButton(
+        'https://paypal.me/tgrosinger',
+        parser.parseFromString(paypal, 'text/xml').documentElement,
+      ),
+    );
+
+    div.appendChild(
+      createDonateButton(
+        'https://www.buymeacoffee.com/tgrosinger',
+        parser.parseFromString(buyMeACoffee, 'text/xml').documentElement,
+      ),
+    );
+  }
+}
+
+const createDonateButton = (link: string, img: HTMLElement): HTMLElement => {
+  const a = document.createElement('a');
+  a.setAttribute('href', link);
+  a.addClass('ledger-donate-button');
+  a.appendChild(img);
+  return a;
+};
