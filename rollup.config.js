09ce57312badb5d06b2c0b6d6ef3083c047d46d5
Tony Grosinger <tony@grosinger.net>
2021-11-28 08:20:30 -0800

    Fix build for mobile

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,11 @@
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
+import replace from "@rollup/plugin-replace";
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import { env } from "process";
 
-const isProd = process.env.BUILD === 'production';
+const isProd = env.BUILD === 'production';
 
 export default {
   input: 'src/main.ts',
@@ -20,9 +22,11 @@ export default {
     resolve({
       browser: true,
     }),
+    replace({
+      "process.env.NODE_ENV": JSON.stringify(env.NODE_ENV),
+    }),
     babel({
       presets: ['@babel/preset-react', '@babel/preset-typescript'],
-      babelHelpers: 'bundled',
     }),
     commonjs(),
   ],
